8e58efbd650be41362fffe02cb3f8834a2f50e25
WeN1nG <3266038380@qq.com>
2026-01-31 19:12:31 +0800

    优化大小

--- build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_calculator_FILE C:/CODE/QTS/Projects/calculator/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/calculator.exe)
-set(__QT_DEPLOY_TARGET_calculator_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_calculator_RUNTIME_DLLS C:/D/software/QT/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/D/software/QT/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/D/software/QT/6.10.1/mingw_64/bin/Qt6Core.dll)
